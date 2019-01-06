708eebb0551057a952bd9edb2aad6fd3a84f2978
wali89 <wali.rahman89@yahoo.com>
2019-01-06 18:02:15 -0500

    minor changes

--- client/src/actions/articleActions.js
+++ client/src/actions/articleActions.js
@@ -1,7 +1,7 @@
 export function fetchTopStories() {
   return (dispatch) => {
     dispatch({type: 'LOADING_ARTICLES'});
-    return fetch('/api/headlines')
+    return fetch('/api/topStories')
       .then(response => response.json())
       .then(articles => dispatch({type: 'FETCH_TOP_STORIES', payload: articles}))
   }
