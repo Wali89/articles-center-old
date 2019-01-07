1636af6614340a4e1618503a29c41d2a1d56fa18
wali89 <wali.rahman89@yahoo.com>
2019-01-07 17:55:54 -0500

    added to channels component

--- client/src/components/channels.js
+++ client/src/components/channels.js
@@ -6,10 +6,20 @@ class Channels extends Component {
   renderSources = () => {
     return (this.props.channels.map((channel, index) => <Channel key={index} id={channel.id} name={channel.name} description={channel.description} url={channel.url} category={channel.category} />))
   }
+
+  render() {
+    <div id ="channels">
+      <div className="ui list">
+      {this.renderSources()}
+      </div>
+    </div>
+  }
 }
 
 const mapStateToProps = state => {
   return {
-    channels : state.channels
+    channels : state.channels,
   }
 }
+
+export default connect(mapStateTopProps)(Channels);
