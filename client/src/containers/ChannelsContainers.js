7cda85fd23f42a35004a28e59223b3ab66076c8f
wali89 <wali.rahman89@yahoo.com>
2019-01-07 19:30:46 -0500

    added fetchChannel

--- client/src/containers/ChannelsContainers.js
+++ client/src/containers/ChannelsContainers.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Channels from '../components/channels';
 import { connect } from 'react-redux';
-import { fetchSources } from '../actions/articleActions';
+import { fetchChannels } from '../actions/articleActions';
 
 
 class ChannelsContainer extends Coponent {
   componentDidMount() {
-    
+    this.props.fetchChannels()
   }
 
   render() {
@@ -24,8 +24,12 @@ const mapStateToProps = state => {
 }
 
 
-const mapDispatchToProps = dispatcj => {
+const mapDispatchToProps = dispatch => {
   return {
-    
+    fetchChannels: () => dispatch(fetchChannels())
   }
 }
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelsContainer);
+
