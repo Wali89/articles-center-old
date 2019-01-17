7b56e5c0b2e624c84c44630b3ff172c73c3660ec
wali89 <wali.rahman89@yahoo.com>
2019-01-16 22:40:44 -0500

    added search controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
   end
 
   def articles_search
-
+    @search = Search.create(article_params)
     response = Faraday.get 'https://newsapi.org/v2/everything?' do |req|
       req.params['apiKey'] = ENV['SECRET_KEY']
       req.params['q'] = params[:article][:query]
